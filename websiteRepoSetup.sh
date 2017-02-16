2d49e1d4300857aabdbec68a581ee7a9f1c66154
Mark Fisher and Ryan McAlpin <mark+ryan@>
2017-02-15 16:53:38 -0800

    add index.html and copy that index.html into the new target directories

--- websiteRepoSetup.sh
+++ websiteRepoSetup.sh
@@ -3,11 +3,13 @@
 mkdir ~/Desktop/$1
 mkdir ~/Desktop/$1/css
 mkdir ~/Desktop/$1/img
+mkdir ~/Desktop/$1/js
 touch ~/Desktop/$1/css/styles.css
-touch ~/Desktop/$1/index.html
+touch ~/Desktop/$1/js/scripts.js
+cp ./index.html ~/Desktop/$1/
 cd ~/Desktop/$1
 git init
 git pair $2 $3
 git add .
-git-pair-commit -m "create initial css folder, img folder, and index.html"
+git-pair-commit -m "create initial css folder with styles.css, js folder with scripts.js, img folder, and index.html"
 atom .
